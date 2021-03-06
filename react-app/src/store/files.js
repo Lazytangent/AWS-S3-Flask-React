33e455356228c157a6efdf8beeb9e84d74a4d461
Lazytangent <pm123633@gmail.com>
2021-03-06 13:44:10 -0600

    Create file store

---
+++ react-app/src/store/files.js
@@ -0,0 +1,48 @@
+const SET_FILE = "files/SET_FILE";
+
+const setFile = (file) => {
+  return {
+    type: SET_FILE,
+    file,
+  };
+};
+
+export const uploadFile = (fileForm) => async (dispatch) => {
+  const {
+    user_id,
+    /* all,
+       other,
+       form,
+       fields, */
+    file, // this is the file for uploading
+  } = fileForm;
+
+  const form = new FormData();
+  form.append('user_id', user_id);
+  // repeat as necessary for each required form field
+  form.append('file', file);
+
+  const res = await fetch('/api/files', {
+    method: "POST",
+    body: form,
+  });
+
+  const uploadedFile = await res.json();
+  if (!uploadedFile.errors) {
+    dispatch(setFile(uploadedFile));
+  }
+  return uploadedFile;
+};
+
+const initialState = {};
+
+const fileReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_FILE:
+      return { ...state, [action.file.id]: action.file }
+    default:
+      return state;
+  }
+};
+
+export default fileReducer;

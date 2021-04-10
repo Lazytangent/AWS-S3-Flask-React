e6d87f13b2b38e56c57c56c7f0eab36913e9e287
Lazytangent <pm123633@gmail.com>
2021-04-09 19:47:44 -0500

    Create getUser

--- react-app/src/store/users.js
+++ react-app/src/store/users.js
@@ -1,9 +1,21 @@
 const SET_USERS = 'users/SET_USERS';
+const SET_USER = 'users/SET_USER';
 
 const setUsers = (users) => ({
   type: SET_USERS,
   users,
 });
+
+const setUser = (user) => ({
+  type: SET_USER,
+  user,
+});
+
+export const getUser = (id) => async (dispatch) => {
+  const res = await fetch(`/api/users/${id}`);
+  const user = await res.json();
+  dispatch(setUser(user));
+};
 
 export const getUsers = () => async (dispatch) => {
   const res = await fetch('/api/users');
@@ -17,6 +29,8 @@ const usersReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_USERS:
       return { ...Object.fromEntries(action.users.map((user) => [user.id, user])) };
+    case SET_USER:
+      return { ...state, [action.user.id]: user };
     default:
       return state;
   }

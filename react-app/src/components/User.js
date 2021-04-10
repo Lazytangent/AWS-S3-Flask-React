e48a9c0da6d3f142f0de80b294ce39ecf06fd6a2
Lazytangent <pm123633@gmail.com>
2021-04-09 19:49:19 -0500

    Configure user component to use Redux

--- react-app/src/components/User.js
+++ react-app/src/components/User.js
@@ -1,22 +1,22 @@
 import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from "react-router-dom";
 
+import { getUser } from '../store/users';
+
 function User() {
-  const [user, setUser] = useState({});
   // Notice we use useParams here instead of getting the params
   // From props.
   const { userId }  = useParams();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.users[userId]);
 
   useEffect(() => {
     if (!userId) {
       return
     }
-    (async () => {
-      const response = await fetch(`/api/users/${userId}`);
-      const user = await response.json();
-      setUser(user);
-    })();
-  }, [userId]);
+    dispatch(getUser(userId));
+  }, [dispatch, userId]);
 
   if (!user) {
     return null;

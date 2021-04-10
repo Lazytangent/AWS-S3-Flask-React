353c34df5687ee9e5d7bf80470c3d7dcc87ee860
Lazytangent <pm123633@gmail.com>
2021-04-09 19:49:59 -0500

    Remove extra import

--- react-app/src/components/User.js
+++ react-app/src/components/User.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from "react-router-dom";
 

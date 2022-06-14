dd8d30a1643513cf33343bcbc2185e8dc81661c4
Lazytangent <peter.tn.mai@gmail.com>
2022-06-13 21:28:31 -0500

    Use fake keys with endpoint

--- app/aws_s3.py
+++ app/aws_s3.py
@@ -7,8 +7,8 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'svg'}
 
 s3 = boto3.client(
     "s3",
-    # aws_access_key_id=Config.S3_KEY,
-    # aws_secret_access_key=Config.S3_SECRET
+    aws_access_key_id=Config.S3_KEY,
+    aws_secret_access_key=Config.S3_SECRET,
     endpoint_url="http://localstack:4566"
 )
 
